3de9c62cfb7202ba48aa959a98a29fc2fb2387da
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-03-20 18:25:33 -0300

    Correct comments on User password validation

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
+# Clase Modelo para los usuarios de la app.
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # Clase Modelo para los usuarios de la app.
 class User < ApplicationRecord
+  # frozen_string_literal: true
   has_secure_password
   validates :username, uniqueness: 'true'
 end
